e76fd0eba3c8145dcdb2eec1746f9dd7e7411285
Nathan Truitt <nathantruitt2@gmail.com>
2021-09-05 17:09:37 -0500

    did some stuff.

--- backup.py
+++ backup.py
@@ -1,2 +1,27 @@
 import os
-from configobj import ConfigObj
+import shutil
+import tarfile
+import logging
+
+# define logging function
+
+
+class Backup:
+
+    def __init__(self, source, destination, logfile, loglevel):
+        self.source = source
+        self.destination = destination
+        self.loglevel = loglevel
+
+
+    def full_file_backup(self, logfile):
+        logformat = '%(asctime)-15s %(message)s'
+        log = logging.basicConfig(format=logformat, filename=logfile)
+        for item in os.listdir(self.source):
+            srcitempath = str(self.source + item)
+            destitempath = str(self.destination + item)
+            try:
+                shutil.copy(src=srcitempath, dst=destitempath, follow_symlinks=True)
+            except OSError:
+                log.error("siwbackup was unable to copy file: {}".format(item))
+
--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 #Main python file
+
+def operators():
+    parser = OptionParser()
+    parser.add_option("-s", "--source", dest="srcfile",
+                      help="Select source directory to back up.")
+    parser.add_option("-d", "--destination", dest="destfile",
+                      help="Select destination directory to save backup.")
+    (srcfile, destfile) = parser.parse_args()
+    return srcfile, destfile
