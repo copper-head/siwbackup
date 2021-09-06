f7fcfc1d96b144d18af15d6e4c5e52242569c6c5
copper-head <asslicker@welikeasses.com>
2021-09-06 09:21:39 -0500

    hi

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
-#Main python file
+# -- module for loading SIW configuration -- #
+#
+#
+# -- Last edited by: Duncan Truitt 9-4-21 -- #
+
+
+import yaml
+
+
+class Config:
+
+    # param:
+    #
+    #   path: Full file path to yaml config file
+
+    def __init__(self, path):
+        self.config_path = path
+
+        # Raise Error if path input != String
+        if type(path) != str:
+            raise TypeError('{} : ---- Config Path input must be a string!'.format(path))
+
+        # Load yaml config file and dump raw contents into a var
+        self.config_file = open(path, 'r')
+        self.config_file_raw = self.config_file.read()
+        self.config_file.close()
+
+        self.yaml_dict = yaml.load(self.config_file_raw, Loader=yaml.FullLoader)
+
+        #TO DO: ADD PORTION TO CHECK ALL ESSENTIAL PORTIONS OF THE YAML FILE EXIST
+
+
+
+
+
+my_loader = Config('example_config.yaml')
+
+print(my_loader.yaml_dict)
